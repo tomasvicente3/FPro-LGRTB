f2d4134abf9058290fefb2a947d79128eb10101d
Tomás Vicente <93838997+tomasvicente3@users.noreply.github.com>
2022-01-04 12:54:07 +0000

    Add files via upload

--- LGTRTB/LGRTB.py
+++ LGTRTB/LGRTB.py
@@ -20,16 +20,19 @@ pot.set_colorkey((255,0,255))
 pg.display.set_icon(pot)
 grid = 32
 
+#fonte
+fonte = pg.font.Font("pricedow.ttf", 40)
+
 #nível
 level = [
-    "0000000000000000000000000",
+    "XxxxXxxxXxxxXxxxXxxxXxxxX",
     "0000000000000000000000000",
     "0000000000000000000000000",
     "0000000000000000000F00000",
     "0000000000000000leaf00000",
     "0000000000000000000000000",
-    "000000000F000000000000000",
-    "000000leaf000000000000000",
+    "00000000FF000000000000000",
+    "00000lleaf000000000000000",
     "0000000000000000000000000",
     "0000000000000000000000000",
     "0000000000000000000000000",
@@ -42,6 +45,7 @@ level = [
     "000000000000000000leaf000",
     "XxxxXxxxXxxxXxxxXxxxXxxxX"
     ]
+bee_start_level = (300, 100)
 
 #background
 bg = pg.transform.scale2x(pg.image.load("Sprites/bg.png"))
@@ -74,12 +78,21 @@ spikes_size = (128, 64) #(4,2)
 bee_ghost = pg.image.load("Sprites/bee_ghost.png")
 bee_ghost.set_colorkey((255,0,255))
 
-#bee_lives = pg.image.load("Sprites/bee_rest.png")
-#bee_lives.set_colorkey((255,0,255))
-#bee_lives.set_alpha(64)
-#bee_mask = pg.image.load("Sprites/bee_mask.png")
-#bee_mask.set_colorkey((0, 0, 0))
-#bee_mask.set_alpha(64)
+#contador de vidas
+bee_lives = pg.image.load("Sprites/bee_rest.png")
+bee_lives.set_colorkey((255,0,255))
+bee_lives.set_alpha(128)
+bee_mask = pg.image.load("Sprites/bee_mask.png")
+bee_mask.set_colorkey((0, 0, 0))
+bee_mask.set_alpha(128)
+
+#contador flores
+flower_counter = pg.image.load("Sprites/flower.png")
+flower_counter.set_colorkey((255,0,255))
+flower_counter.set_alpha(128)
+flower_mask = pg.image.load("Sprites/flower_mask.png")
+flower_mask.set_colorkey((0, 0, 0))
+flower_mask.set_alpha(128)
 
 #relógio
 relogio = pg.image.load("Sprites/clock.png")
@@ -101,19 +114,24 @@ bee_vy = 0
 
 k = 4
 bee = [bee_rest]*k + [bee_fly]*k + [bee_ghost]
-state = 0
+bee_state = 0
 
 alive = True
 res = 0
 res_time = 500
-lives = 3
 
-flower_cont = 7337
+#inic. vars HUD
+flower_count_raw = 1337
+score_raw = 00
+time_raw = 120 #segundos
+lives_raw = 3
 
 left_key = right_key = up_key = False
 in_leaf = False
 clock = pg.time.Clock()
 
+#contagem tempo
+pg.time.set_timer(pg.USEREVENT, 1000, 9999999)
 
 running = True
 while running:
@@ -138,22 +156,25 @@ while running:
                 right_key = False
             elif ev.key == pg.K_UP or ev.key == pg.K_w:
                 up_key = False
+        if ev == pg.USEREVENT:
+            time_raw -= 1
 
     #leis
 
     dt = clock.tick()
     if alive:
         if up_key:
-            bee_vy = -0.35
-            state = (state+1)%(2*k)
+            bee_vy = -0.3
+            bee_state = (bee_state+1)%(2*k)
+            in_leaf = False
         elif not in_leaf:
-            bee_vy = 0.05*dt
-            state = 0
+            bee_vy = 0.3
+            bee_state = 0
 
         if left_key:
-            bee_vx = -0.35
+            bee_vx = -0.3
         elif right_key:
-            bee_vx = 0.35
+            bee_vx = 0.3
         else:
             bee_vx = 0.0
 
@@ -161,13 +182,6 @@ while running:
     bee_oldy = bee_y
     bee_x += bee_vx*dt
     bee_y += bee_vy*dt
-
-    if bee_y>screen_size[1]-bee_size: #limite chão (temporário)
-        bee_y = screen_size[1]-bee_size
-        bee_vy = 0
-        in_leaf = True
-    else:
-        in_leaf = False
 
     if bee_x < 0: #limites verticais do ecrã
         bee_x = 0
@@ -178,7 +192,7 @@ while running:
     for row in range(0, len(level)):
         for col in range(0, len(level[row])):
             #colisão folhas
-            if level[row][col] in 'leaf':
+            if level[row][col] in 'leaf' and alive:
                 if bee_x + bee_size >= col * grid and bee_x <= (col+1) * grid:
                     #limite superior
                     if bee_y + bee_size >= row * grid and bee_oldy < row * grid and bee_vy > 0:
@@ -189,6 +203,8 @@ while running:
                     if bee_y < (row+1) * grid and bee_oldy >= (row+1) * grid and bee_vy < 0:
                         bee_vy = 0
                         bee_y = (row+1) * grid
+                else:
+                    in_leaf = False
                 if bee_y + bee_size > row * grid and bee_y < (row+1) * grid:
                     #limite esquerdo
                     if bee_x + bee_size >= (col) * grid and bee_oldx < (col) * grid and bee_vx > 0:
@@ -201,60 +217,93 @@ while running:
 
             #apanhar flores
             if level[row][col] == 'F':
-                if col*grid-bee_size < bee_x < (col+1)*grid and row*grid-bee_size < bee_y < row*grid:
-                    level[row] = level[row][:col] + "0" + level[row][col+1:]
+                if col*grid-bee_size < bee_x < (col+1)*grid and row*grid-bee_size < bee_y < row*grid and alive:
+                    level[row] = level[row][:col] + "C" + level[row][col+1:]
+                    score_raw += 20
 
             #espinhos
             if level[row][col] in "Xx":
-                if col*grid-bee_size < bee_x < (col+1)*grid and row*grid-bee_size < bee_y < row*grid:
+                if col*grid-bee_size < bee_x < (col+1)*grid and row*grid-bee_size < bee_y < (row+1)*grid:
                     bee_vy = -0.01
-                    bee_vx = 0.01*math.cos(10*pg.time.get_ticks())
                     alive = False
 
+
+    #sequencia de respawn
     if not alive:
         if res < res_time:
-            state = 2*k
+            bee_vx = 0.05*math.sin(pg.time.get_ticks()/200)
+            bee_state = 2*k
             res +=1
         else:
             alive = True
-            lives -= 1
-            bee_x = 300
-            bee_y = 100
-            state = 0
+            lives_raw -= 1
+            time_raw = 120
+            bee_x, bee_y = bee_start_level
+            bee_state = 0
             res = 0
 
-    if lives == 0:
+    #game over por vidas
+    if lives_raw == 0:
         running = False
 
-    if flower_cont == 0:
+    #morte por tempo
+    if time_raw == 0:
+        alive = False
+
+    #reset do nivel (temporário)
+    if flower_count_raw == 0:
         for row in range(0, len(level)):
             for col in range(0, len(level[row])):
-                if level[row][col] == 'f':
-                    level[row-1] = level[row-1][:col] + "F" + level[row][col+1:]
+                if level[row][col] == 'C':
+                    level[row] = level[row][:col] + "F" + level[row][col+1:]
 
-    #desenho
+    #desvio texto pontuação
+    score_x_var = 0
+    for i in range(0,len(str(score_raw))):
+        score_x_var += 5
+
+#desenho
+
+    #papel de fundo
     for x in range(0, screen_size[0]//bg_size+2):
         for y in range(0, screen_size[1]//bg_size+2):
             screen.blit(bg, (x*bg_size ,y*bg_size))
 
-    flower_cont = 0
+    #criador de nível (c/ contador de flores restantes)
+    flower_count_raw = 0
     for row in range(0, len(level)):
         for col in range(0, len(level[row])):
             if level[row][col] == 'l':
                 screen.blit(leaf, (col*grid, row*grid))
             elif level[row][col] == 'F':
-                flower_cont += 1
+                flower_count_raw += 1
                 screen.blit(flower, (col*grid, row*grid))
             elif level[row][col] == 'X':
                 screen.blit(spikes, (col*grid, row*grid))
 
+    #texto
+    flower_count = pg.font.Font.render(fonte, str(flower_count_raw), True, (255, 194, 0))
+    lives = pg.font.Font.render(fonte, str(lives_raw), True, (255, 194, 0))
+    time = pg.font.Font.render(fonte, str(time_raw), True, (255, 194, 0))
+    score = pg.font.Font.render(fonte, str(score_raw), True, (255, 194, 0))
 
-    screen.blit(bee[state], (bee_x, bee_y))
+    #abelha :D
+    screen.blit(bee[bee_state], (bee_x, bee_y))
 
-#    screen.blit(bee_mask, (screen_size[0]-64, screen_size[1]-96))
-#    screen.blit(bee_rest, (screen_size[0]-64, screen_size[1]-96))
+    #HUD
+    screen.blit(bee_mask, (screen_size[0]-96, screen_size[1]-80))
+    screen.blit(bee_lives, (screen_size[0]-96, screen_size[1]-80))
+    screen.blit(lives, (screen_size[0]-72, screen_size[1]-55))
+
+    screen.blit(flower_mask, (32, screen_size[1]-64))
+    screen.blit(flower_counter, (32, screen_size[1]-64))
+    screen.blit(flower_count, (40, screen_size[1]-55))
+
     screen.blit(relogio, (screen_size[0]/2-16, screen_size[1]-64))
+    screen.blit(time, (screen_size[0]/2-16, screen_size[1]-55))
+
     screen.blit(hive, (screen_size[0]/2-16, 32))
+    screen.blit(score, (screen_size[0]/2-3-score_x_var, 40))
 
 
     pg.display.flip()
